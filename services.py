7c418baac3eb966d395b3042dd4f031bb79a8925
Sergey Karatach <karatach1998@yandex.ru>
2021-03-13 16:38:50 +0300

    Fix yaml.load deprecation warning

--- services.py
+++ services.py
@@ -1,16 +1,16 @@
 import random
 from collections import OrderedDict
 
-import yaml
 import wolframalpha
+import yaml
 from sympy.parsing.latex import parse_latex
 from sympy.printing.maple import maple_code
 
 import config
-from books import Book
-from math_problems import MathProblem, MathTask, BookRef
-from russian_rules import RussianRules
 from book_stores import BOOK_STORE_CONTAINER
+from books import Book
+from math_problems import BookRef, MathProblem, MathTask
+from russian_rules import RussianRules
 from utils import latex2png
 
 
@@ -30,15 +30,17 @@ class MathProblemsService:
                     chapter_title=filepath.stem,
                     book_ref=BookRef(**task.pop("book_ref")),
                     **task,
-                )
-                for task in yaml.load(filepath.read_text())
+                ) for task in yaml.safe_load(filepath.read_text())
             ]
 
         assert set(problems_dir.glob("*.yaml")) == set(problems_dir.iterdir())
         self._problems = {
-            p.stem.split(maxsplit=1)[1]: load_tasks(p) for p in problems_dir.iterdir()
+            p.stem.split(maxsplit=1)[1]: load_tasks(p)
+            for p in problems_dir.iterdir()
         }
-        self._book = Book(BOOK_STORE_CONTAINER, **yaml.load(book_path.read_text()))
+        self._book = Book(
+            BOOK_STORE_CONTAINER, **yaml.safe_load(book_path.read_text())
+        )
 
     chapter_titles = property(lambda self: list(self._problems.keys()))
 
@@ -49,7 +51,9 @@ class MathProblemsService:
         random_task = random.choice(tasks)
         random_problem = random.choice(random_task.problems)
         return MathProblem(
-            task=random_task.task, problem=random_problem, book_ref=random_task.book_ref
+            task=random_task.task,
+            problem=random_problem,
+            book_ref=random_task.book_ref
         )
 
     def get_problem_img(self, problem):
@@ -63,7 +67,7 @@ class MathProblemsService:
         res = self._wa_client.query(query)
         for pod in res.results:
             try:
-                yield (f"_WolframAlpha:_ {pod.title}", next(next(pod.subpod).img).src)
+                yield pod.title, next(next(pod.subpod).img).src
             except (StopIteration, AttributeError):
                 continue
 
@@ -78,7 +82,10 @@ class MathProblemsService:
             theory_pdf = self._book.get_page_range_pdf(start, end)
         else:
             chapter = next(
-                filter(lambda ch: ch["title"] == chapter_title, self._book.chapters)
+                filter(
+                    lambda ch: ch["title"] == chapter_title,
+                    self._book.chapters
+                )
             )
             theory_pdf = self._book.get_chapter_pdf(chapter)
 
@@ -101,11 +108,15 @@ class IntegralProblemsService(MathProblemsService):
 class RussianRulesService:
     def __init__(self):
         metainfo_path = config.RESOURCES_PATH / "russian_rules.yaml"
-        self._russian_rules = RussianRules(**yaml.load(metainfo_path.read_text()))
+        self._russian_rules = RussianRules(
+            **yaml.safe_load(metainfo_path.read_text())
+        )
 
     part_titles = property(lambda self: self._russian_rules.part_titles)
 
-    def get_random_paragraph_html(self, part_title):
+    def get_random_paragraph_html(
+        self, part_title, *, return_chapter_title=False
+    ):
         random_chapter = random.choice(
             self._russian_rules.list_part_chapters(part_title)
         )
@@ -113,23 +124,33 @@ class RussianRulesService:
             random_chapter
         )
         paragraph_html = random.choice(paragraphs_html)
-        return paragraph_html
+        if not return_chapter_title:
+            return paragraph_html
+        else:
+            return random_chapter['title'], paragraph_html
 
 
 class BookCollectionService:
     def __init__(self):
         def load_book(path):
-            return Book(BOOK_STORE_CONTAINER, **yaml.load(path.read_text()))
+            return Book(
+                BOOK_STORE_CONTAINER, **yaml.safe_load(path.read_text())
+            )
 
         self._books = OrderedDict(
             list(
-                map(lambda b: (b.title, b), map(load_book, config.BOOKS_PATH.iterdir()))
+                map(
+                    lambda b: (b.title, b),
+                    map(load_book, config.BOOKS_PATH.iterdir())
+                )
             )
         )
 
     book_titles = property(lambda self: self._books.keys())
 
-    def get_random_book_chapter_pdf(self, book_title, *, return_title_tuple=False):
+    def get_random_book_chapter_pdf(
+        self, book_title, *, return_title_tuple=False
+    ):
         book = self._books[book_title]
 
         random_part = book.parts and random.choice(book.parts)
